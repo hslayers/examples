68fe0b520f881e3d43b2f0be604a78a049483669
fzadrazil <fzadr@LAPTOP-FARNK195>
2020-06-15 12:16:55 +0200

    Fixed WMTS example webpack config

--- wmts-vector-tiles/webpack.prod.js
+++ wmts-vector-tiles/webpack.prod.js
@@ -109,7 +109,7 @@ module.exports = merge(common, {
       // AngularJS templates are cached using cache template
       {
         test: /\.html$/,
-        exclude: path.resolve(__dirname, 'src/index.html'),
+        exclude: path.resolve(__dirname, 'index.html'),
         use: [
           'ng-cache-loader?prefix=[dir]/[dir]',
           'extract-loader',
