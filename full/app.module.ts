6aa08cf3f17eda6d55d38960de76f88597aea840
raitis <raitisbe@gmail.com>
2020-06-17 18:42:22 +0300

    Inject only HsCore and other components will follow

--- full/app.module.ts
+++ full/app.module.ts
@@ -6,10 +6,6 @@ import { NgModule, ComponentRef, ApplicationRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UpgradeModule } from '@angular/upgrade/static';
 import app from './app-js';
-import { HsMapServiceProvider, HsLayoutServiceProvider, HsUtilsServiceProvider } from 'hslayers-ng/ajs-upgraded-providers';
-import { HsConfigProvider } from 'hslayers-ng/ajs-upgraded-providers';
-import { HsLayoutModule } from 'hslayers-ng/components/layout/layout.module';
-import { HsLegendComponent } from 'hslayers-ng/components/legend/legend.component';
 import { APP_BOOTSTRAP_LISTENER } from '@angular/core';
 import { BootstrapComponent } from 'hslayers-ng/bootstrap.component';
 import { HsCoreModule } from 'hslayers-ng/components/core/core.module';
@@ -17,19 +13,15 @@ import { HsCoreModule } from 'hslayers-ng/components/core/core.module';
     imports: [
         BrowserModule,
         UpgradeModule,
-        HsLayoutModule,
         HsCoreModule
     ],
-    declarations: [
-
-    ],
-    entryComponents: [
-
-    ],
-    providers: [HsMapServiceProvider, HsConfigProvider, HsUtilsServiceProvider,
+    declarations: [],
+    entryComponents: [],
+    providers: [
         {
             provide: APP_BOOTSTRAP_LISTENER, multi: true, useFactory: () => {
                 return (component: ComponentRef<BootstrapComponent>) => {
+                    //When ng9 part is bootstrapped continue with AngularJs modules
                     component.instance.upgrade.bootstrap(document.documentElement, [app.name], { strictDi: true });
                 }
             }
@@ -38,6 +30,7 @@ import { HsCoreModule } from 'hslayers-ng/components/core/core.module';
 export class AppModule {
     constructor() { }
     ngDoBootstrap(appRef: ApplicationRef) {
+        //First bootstrap Angular 9 app part on hs element
         appRef.bootstrap(BootstrapComponent);
     }
 }
