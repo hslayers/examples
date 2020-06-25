4dbae6f5ef6e3be8451cab56eee19c2854461568
raitis <raitisbe@gmail.com>
2020-06-25 13:45:44 +0300

    Fix webpack prod: html-loader, ng-annotate, s-maps

--- full/webpack.prod.js
+++ full/webpack.prod.js
@@ -22,7 +22,7 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: false,
+  devtool: 'source-map',
   output: {
     // Add a chunkhash to file name so it will not be cached by browsers when content changed
     filename: '[name].[hash].bundle.js',
@@ -53,6 +53,7 @@ module.exports = merge(common, {
       // JS minifier/uglifier
       new TerserPlugin({
         parallel: true,
+        sourceMap: true,
         // Remove comments as well
         terserOptions: {output: {comments: false}},
       }),
@@ -106,14 +107,16 @@ module.exports = merge(common, {
           },
         ],
       },
-      // AngularJS templates are cached using cache template
       {
         test: /\.html$/,
         exclude: path.resolve(__dirname, 'src/index.html'),
         use: [
           {
             loader: 'html-loader',
-            options: {minimize: true},
+            options: {
+              minimize: true,
+              caseSensitive: true,
+            },
           },
         ],
       },
