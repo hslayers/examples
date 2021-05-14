a0654b3ba0408f1834c6334b744e05076f941d67
raitis <raitisbe@gmail.com>
2021-05-14 16:03:03 +0300

    feat: Add hslayers-cesium-app example

---
+++ cesium/appexternal.js
@@ -0,0 +1,14 @@
+function hslayersNgConfig(ol) {
+  return {
+    assetsPath: '../node_modules/hslayers-cesium-app/assets/',
+    cesiumBase: '../node_modules/hslayers-cesium-app/assets/cesium/',
+    default_layers: [],
+
+    default_view: new ol.View({
+      center: ol.proj.transform([17.474129, 52.574], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
+      zoom: 4,
+      units: 'm',
+    }),
+  };
+}
+
