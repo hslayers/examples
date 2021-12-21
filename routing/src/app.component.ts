101d27b124a9b0dc85a1da52d70bebf099d78e06
raitis <raitisbe@gmail.com>
2021-12-21 15:53:57 +0200

    fix: Building of routing example

--- routing/src/app.component.ts
+++ routing/src/app.component.ts
@@ -19,27 +19,35 @@ export class AppComponent {
       popUpDisplay: 'click',
       box_layers: [
         new Group({
-          title: 'Base layer',
+          properties: {
+            title: 'Base layer',
+          },
           layers: [
             new Tile({
               source: new OSM(),
-              title: 'OpenStreetMap',
-              base: true,
-              visible: true,
-              removable: false,
+              properties: {
+                title: 'OpenStreetMap',
+                base: true,
+                removable: false,
+              },
+              visible: true,             
             }),
             new Tile({
-              title: 'OpenCycleMap',
-              visible: false,
-              base: true,
+              properties: {
+                title: 'OpenCycleMap',
+                base: true,
+              },
+              visible: false,             
               source: new OSM({
                 url: 'http://{a-c}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
               }),
             }),
             new Tile({
-              title: 'Satellite',
+              properties: {
+                title: 'Satellite',
+                base: true,
+              },
               visible: false,
-              base: true,
               source: new XYZ({
                 url:
                   'http://api.tiles.mapbox.com/v4/mapbox.streets-satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicmFpdGlzYmUiLCJhIjoiY2lrNzRtbGZnMDA2bXZya3Nsb2Z4ZGZ2MiJ9.g1T5zK-bukSbJsOypONL9g',
@@ -51,7 +59,6 @@ export class AppComponent {
       default_view: new View({
         center: transform([17.474129, 52.574], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
         zoom: 4,
-        units: 'm',
       }),
       panelsEnabled: {
         tripPlanner: true,
