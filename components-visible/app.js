7af3e6a461d2248228664073dabe4695f624a8f5
jmacura <macurajan@gmail.com>
2024-08-14 11:05:38 +0200

    fix(components): Revive components-visible example

--- components-visible/app.js
+++ components-visible/app.js
@@ -1,91 +1,58 @@
-'use strict';
-import 'toolbar.module';
-import 'print.module';
-import 'query.module';
-import 'search.module';
-import 'measure.module';
-import 'permalink.module';
-import 'info.module';
-import 'datasource-selector.module';
-import 'sidebar.module';
-import 'add-layers.module';
-import 'draw.module';
-import {Tile, Group} from 'ol/layer';
-import {OSM} from 'ol/source';
-import View from 'ol/View';
-import {transform} from 'ol/proj';
-
-angular.module('hs', [
-  'hs.sidebar',
-  'hs.toolbar',
-  'hs.draw',
-  'hs.layermanager',
-  'hs.map',
-  'hs.query',
-  'hs.search', 'hs.print', 'hs.permalink', 'hs.measure',
-  'hs.legend', 'hs.core',
-  'hs.datasource_selector',
-  'hs.save-map',
-  'hs.addLayers',
-  'gettext',
-  'hs.compositions',
-  'hs.info'
-])
-
-  .directive('hs', ['config', 'Core', function (config, Core) {
-    return {
-      template: Core.hslayersNgTemplate,
-      link: function (scope, element) {
-        Core.fullScreenMap(element);
-      }
-    };
-  }])
-
-  .value('config', {
-    importCss: true,
-    proxyPrefix: '/proxy/',
+function hslayersNgConfig(ol) {
+  return {
+    useProxy: false,
+    assetsPath: '../node_modules/hslayers-ng-app/assets/',
     open_lm_after_comp_loaded: true,
-    layer_order: '-position',
-    box_layers: [
-      new Group({
-        title: 'Base layer',
-        layers: [
-          new Tile({
-            source: new OSM(),
-            title: 'OpenStreetMap',
-            base: true,
-            visible: true,
-            removable: false
-          })
-        ]
-      })
-    ],
     default_layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+        properties: {
+          title: 'OpenStreetMap',
+          base: true,
+          removable: false,
+          thumbnail: 'osm.png',
+        },
+        visible: true,
+      }),
     ],
-    componentsEnabled: {
-      sidebar: true,
-      toolbar: true,
-      guiOverlay: true,
-      drawToolbar: true,
-      searchToolbar: true,
-      measureToolbar: true,
-      golocationButton: false,
-      mapControls: false
-    },
-    mapInteractionsEnabled: true,
-    default_view: new View({
-      center: transform([17.474129, 52.574000], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
+    default_view: new ol.View({
+      center: ol.proj.transform([17.474129, 52.574000], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
       zoom: 4,
-      units: 'm'
+      units: 'm',
     }),
-    allowAddExternalDatasets: true,
-    compositions_catalogue_url: '/php/catalogue/libs/cswclient/cswClientRun.php',
-    status_manager_url: '/wwwlibs/statusmanager2/index.php',
-    datasources: []
-  })
-
-  .controller('MainController', [
-    function () {
-    }
-  ]);
-
+    // sidebarPosition: 'invisible', /* uncomment to hide sidebar */
+    componentsEnabled: {
+      guiOverlay: true, //all GUI components at once
+      info: true, //composition loading info
+      toolbar: true, //all toolbars at once: draw, search and measure
+      drawToolbar: true, //toolbar to draw/edit features
+      searchToolbar: false, //toolbar with search field
+      measureToolbar: true, //toolbar to measure distances/areas
+      geolocationButton: false, //button which allows to geolocate the user and optionally follow his/her position
+      defaultViewButton: true, //button to zoom and pan to an initial extent
+      mapControls: false, //buttons to zoom in/out and the scale
+      basemapGallery: true, //Move basemaps list into separate GUI component located in the map
+      mapSwipe: false, //Enables map-swipe right after app starts
+      queryPopup: true, //Enables queryPopup functionality
+    },
+    panelsEnabled: {
+      compositions: false,
+      query: true,
+      draw: false,
+      addData: false,
+      layerManager: true,
+      legend: true,
+      print: true,
+      saveMap: true,
+      language: true,
+      share: false,
+      feature_crossfilter: true,
+      featureTable: true,
+      tracking: false,
+      tripPlanner: true,
+      mapSwipe: false, //A component for splitting a map into two parts with a slider, allowing the user to display the contents of different layers in 
+      //parallel on top of each other.
+      sensors: false,
+    },
+  };
+}
